691cd290cdbd944ae534bbc7f1b1b18a7cc7fad4
Artur Khareksian <r2mail1122@gmail.com>
2024-07-21 19:23:58 +0300

    feat: ass trends sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,24 +1,26 @@
+import TrendsSidebar from "@/components/TrendsSidebar"
 import Post from "@/components/posts/Post"
 import PostEditor from "@/components/posts/editor/PostEditor"
 import prisma from "@/lib/prisma"
-import { PostDataInclude } from "@/lib/types"
+import { postDataInclude } from "@/lib/types"
 
 export default async function Home() {
   const posts = await prisma.post.findMany({
-    include: PostDataInclude,
+    include: postDataInclude,
     orderBy: {
       createdAt: "desc",
     },
   })
 
   return (
-    <main className="min-w-00 h-[200vh] w-full">
+    <main className="flex w-full min-w-0">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   )
 }
